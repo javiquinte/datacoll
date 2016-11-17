689734088524303da805e0e854810ba58debd266
Javier Quinteros <javiquinte@gmail.com>
2016-11-17 13:24:29 +0100

    Bugfix: Environment was not included in the parameters of features.

--- modules/features.py
+++ modules/features.py
@@ -2,12 +2,13 @@ import json
 
 class DC_Module(object):
     def __init__(self, dc):
+        # dc is supposed to be an instance of a DCApp class
         dc.registerAction("/features", self.features)
 
         # We keep a copy of it
         self.__dc = dc
 
-    def features(self):
+    def features(self, environ):
         """Read the features of the system and return them in JSON format.
 
         :returns: System capabilities in JSON format
