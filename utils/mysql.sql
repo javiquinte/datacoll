0fb98552a4a0d692812e74cb5c1c5a5d54f030dc
Javier Quinteros <javiquinte@gmail.com>
2016-11-28 22:31:03 +0100

    Add 6 more chars to the column in table to include the prefix and the backslash.

--- utils/mysql.sql
+++ utils/mysql.sql
@@ -10,7 +10,7 @@ CREATE TABLE user (
 
 CREATE TABLE collection (
         id INT AUTO_INCREMENT,
-        pid VARCHAR(36) DEFAULT NULL,
+        pid VARCHAR(42) DEFAULT NULL,
         owner INT NOT NULL,
         ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
         PRIMARY KEY(id),
@@ -20,7 +20,7 @@ CREATE TABLE collection (
 CREATE TABLE member (
         id INT AUTO_INCREMENT,
         cid INT NOT NULL,
-        pid VARCHAR(36) NOT NULL,
+        pid VARCHAR(42) NOT NULL,
         checksum VARCHAR(50),
         PRIMARY KEY(id),
         UNIQUE (cid, id),
@@ -33,6 +33,6 @@ CREATE TABLE member (
 INSERT INTO user (id, mail) VALUES(1, 'javier@gfz-potsdam.de');
 INSERT INTO collection (pid, owner) VALUES('uuid-collection', 1);
 INSERT INTO collection (pid, owner) VALUES('uuid-collection2', 1);
-INSERT INTO member (cid, pid, checksum) VALUES (1, 'BFEF7EDB-48E0-11E6-AC84-82F855C2CCB7', 'sha2:DAqRlSpLE13O4ECLLAGUN0EZMX0bQqUB8JqprCDfFLE=');
-INSERT INTO member (cid, pid, checksum) VALUES (1, '331FF9D0-48F6-11E6-AC84-82F855C2CCB7', 'sha2:OIDACYr2jLpcwi2Ba0cszKUJOIQB00yAi7+rEAWRBb0=');
-INSERT INTO member (cid, pid, checksum) VALUES (2, '331FF9D0-48F6-11E6-AC84-82F855C2CCB7', 'sha2:OIDACYr2jLpcwi2Ba0cszKUJOIQB00yAi7+rEAWRBb0=');
+INSERT INTO member (cid, pid, checksum) VALUES (1, '11708/BFEF7EDB-48E0-11E6-AC84-82F855C2CCB7', 'sha2:DAqRlSpLE13O4ECLLAGUN0EZMX0bQqUB8JqprCDfFLE=');
+INSERT INTO member (cid, pid, checksum) VALUES (1, '11708/331FF9D0-48F6-11E6-AC84-82F855C2CCB7', 'sha2:OIDACYr2jLpcwi2Ba0cszKUJOIQB00yAi7+rEAWRBb0=');
+INSERT INTO member (cid, pid, checksum) VALUES (2, '11708/331FF9D0-48F6-11E6-AC84-82F855C2CCB7', 'sha2:OIDACYr2jLpcwi2Ba0cszKUJOIQB00yAi7+rEAWRBb0=');
