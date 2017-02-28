ef5f65729e28352ed7fe34aa06ccb0cc0a75321f
Javier Quinteros <javiquinte@gmail.com>
2017-02-28 17:58:52 +0100

    Add first test case. Creation and deletion of collections.

---
+++ tests/test01.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+"""Tests to check the minimum Data Collection Service functionality is OK.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+   :Copyright:
+       2016-2017 Javier Quinteros, GEOFON, GFZ Potsdam <geofon@gfz-potsdam.de>
+   :License:
+       GPLv3
+   :Platform:
+       Linux
+
+.. moduleauthor:: Javier Quinteros <javier@gfz-potsdam.de>, GEOFON, GFZ Potsdam
+"""
+
+import sys
+import os
+import unittest
+import urllib2
+import json
+
+here = os.path.dirname(__file__)
+sys.path.append(os.path.join(here, '..'))
+from unittestTools import WITestRunner
+
+
+class DataCollTests(unittest.TestCase):
+    """Test the functionality of the Data Collection Service
+
+    """
+    @classmethod
+    def setUp(cls):
+        "Setting up test"
+        cls.host = host
+
+    def test_coll_create_delete(self):
+        "creation and deletion of a Collection"
+
+	with open('new-coll.json') as fin:
+            data = fin.read()
+        req = urllib2.Request('%s/collections' % self.host, data=data)
+        req.add_header("Content-Type",'application/json')
+        # Create a collection
+        try:
+            u = urllib2.urlopen(req)
+            coll = json.loads(u.read())
+            # FIXME I skip the check of the 201 HTTP Error code but I shouldn't
+            self.assertEqual(u.getcode(), 201, 'HTTP code 201 was expected!')
+        except Exception as e:
+            self.assertTrue(False, 'Error: %s' % e)
+
+        req = urllib2.Request('%s/collections/%d' % (self.host, coll['id']))
+        req.get_method = lambda: 'DELETE'
+        u = urllib2.urlopen(req)
+        self.assertEqual(u.getcode(), 200)
+        return
+
+global host
+
+host = 'http://localhost:8080/rda/datacoll'
+
+if __name__ == '__main__':
+
+    # 0=Plain mode (good for printing); 1=Colourful mode
+    mode = 1
+
+    # The default host is localhost
+    for ind, arg in enumerate(sys.argv):
+        if arg in ('-p', '--plain'):
+            del sys.argv[ind]
+            mode = 0
+        elif arg == '-u':
+            host = sys.argv[ind + 1]
+            del sys.argv[ind + 1]
+            del sys.argv[ind]
+        elif arg in ('-h', '--help'):
+            usage()
+            sys.exit(0)
+
+    unittest.main(testRunner=WITestRunner(mode=mode))
