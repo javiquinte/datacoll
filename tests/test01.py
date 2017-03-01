a69423f618def9bea2ab44977820a17f8d709881
Javier Quinteros <javiquinte@gmail.com>
2017-03-01 15:56:54 +0100

    Add a test for the creation, deletion and query of members.

--- tests/test01.py
+++ tests/test01.py
@@ -56,6 +56,75 @@ class DataCollTests(unittest.TestCase):
 
         return
 
+    def test_memb_create_query_delete(self):
+        "creation, query and deletion of a Member of a Collection"
+
+	with open('new-coll.json') as fin:
+            data = fin.read()
+        req = urllib2.Request('%s/collections' % self.host, data=data)
+        req.add_header("Content-Type",'application/json')
+        # Create a collection
+        try:
+            u = urllib2.urlopen(req)
+            coll = json.loads(u.read())
+            # Check that I received a 201 code
+            self.assertEqual(u.getcode(), 201, 'HTTP code 201 was expected!')
+        except Exception as e:
+            self.assertTrue(False, 'Error: %s' % e)
+
+	with open('new-memb.json') as fin:
+            data = fin.read()
+        req = urllib2.Request('%s/collections/%d/members' %
+                              (self.host, coll['id']), data=data)
+        req.add_header("Content-Type",'application/json')
+        # Create a member
+        try:
+            u = urllib2.urlopen(req)
+            memb = json.loads(u.read())
+            # Check that I received a 201 code
+            self.assertEqual(u.getcode(), 201, 'HTTP code 201 was expected!')
+        except Exception as e:
+            self.assertTrue(False, 'Error: %s' % e)
+
+        # Query the member to check it has been properly created
+        req = urllib2.Request('%s/collections/%d/members/%d' %
+                              (self.host, coll['id'], memb['id']))
+        try:
+            u = urllib2.urlopen(req)
+            memb2 = json.loads(u.read())
+            # Check that the ids are the same
+            self.assertEqual(memb['id'], memb2['id'], 'IDs differ!')
+            # Compare owner with the original one
+            self.assertEqual(json.loads(data)['location'], memb2['location'],
+                             'Location recorded differ with the original one!')
+        except Exception as e:
+            self.assertTrue(False, 'Error: %s' % e)
+
+        # FIXME Check the syntax of "properties"
+        # Query the collection capabilities
+        # req = urllib2.Request('%s/collections/%d/capabilities' % (self.host, coll['id']))
+        # try:
+        #     u = urllib2.urlopen(req)
+        #     capab = json.loads(u.read())
+        #     # Check that the capabilities have at least the maxLength field
+        #     self.assertEqual(capab['maxLength'], -1, 'maxLength is supposed to be -1 for this test!')
+        # except Exception as e:
+        #     self.assertTrue(False, 'Error: %s' % e)
+
+        # Delete the member
+        req = urllib2.Request('%s/collections/%d/members/%d' %
+                              (self.host, coll['id'], memb['id']))
+        req.get_method = lambda: 'DELETE'
+        u = urllib2.urlopen(req)
+        self.assertEqual(u.getcode(), 200)
+
+        # Delete the collection
+        req = urllib2.Request('%s/collections/%d' % (self.host, coll['id']))
+        req.get_method = lambda: 'DELETE'
+        u = urllib2.urlopen(req)
+        self.assertEqual(u.getcode(), 200)
+        return
+
     def test_coll_create_query_delete(self):
         "creation, query and deletion of a Collection"
 
