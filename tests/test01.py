2da2d59e75c0e37850eaf8e7bb12d0dec75f99de
Javier Quinteros <javier@quinteros.eu>
2017-10-30 09:57:32 +0100

    Refactorize completely the code of tests.

--- tests/test01.py
+++ tests/test01.py
@@ -31,6 +31,116 @@ sys.path.append(os.path.join(here, '..'))
 from unittestTools import WITestRunner
 
 
+def createcollection(baseurl, datafile):
+    with open(datafile) as fin:
+        data = fin.read().encode()
+        req = Request('%s/collections' % baseurl, data=data)
+        req.add_header("Content-Type", 'application/json')
+        # Create a collection
+        u = urlopen(req)
+        coll = json.loads(u.read())
+        # Check that I received a 201 code
+
+        if u.getcode() == 201:
+            return coll['collid']
+
+    raise Exception('Collection not created.')
+
+
+def createmember(baseurl, collid, datafile):
+    with open(datafile) as fin:
+        data = fin.read().encode()
+        req = Request('%s/collections/%s/members' % (baseurl, collid), data=data)
+        req.add_header("Content-Type", 'application/json')
+        # Create a member
+        u = urlopen(req)
+        memb = json.loads(u.read())
+        # Check that I received a 201 code
+
+        if u.getcode() == 201:
+            return memb['memberid']
+
+    raise Exception('Member not created.')
+
+
+def deletemember(baseurl, collid, memberid):
+    # Delete a member of a collection
+    req = Request('%s/collections/%d/members/%d' %
+                  (baseurl, collid, memberid))
+    req.get_method = lambda: 'DELETE'
+
+    u = urlopen(req)
+    # Check that error code is 200
+    if u.getcode() == 200:
+        return True
+
+    raise Exception('Error deleting member.')
+
+
+def deletecollection(baseurl, collid):
+    # Delete a collection
+    req = Request('%s/collections/%d' %
+                  (baseurl, collid))
+    req.get_method = lambda: 'DELETE'
+
+    u = urlopen(req)
+    # Check that error code is 200
+    if u.getcode() == 200:
+        return True
+
+    raise Exception('Error deleting collection.')
+
+
+def getmember(baseurl, collid, memberid=None):
+    # Query the member to check it has been properly created
+    if memberid is not None:
+        req = Request('%s/collections/%d/members/%d' %
+                      (baseurl, collid, memberid))
+    else:
+        req = Request('%s/collections/%d/members' %
+                      (baseurl, collid))
+
+    u = urlopen(req)
+    memb = json.loads(u.read())
+    # Check that error code is 200
+    if u.getcode() == 200:
+        return memb
+
+    raise Exception('Error retrieving member(s).')
+
+
+def getcollection(baseurl, collid=None):
+    # Query the collection to check it has been properly created
+    if collid is not None:
+        req = Request('%s/collections/%d' %
+                      (baseurl, collid))
+    else:
+        req = Request('%s/collections' %
+                      (baseurl))
+
+    u = urlopen(req)
+    coll = json.loads(u.read())
+    # Check that error code is 200
+    if u.getcode() == 200:
+        return coll
+
+    raise Exception('Error retrieving collection(s).')
+
+
+def getcollcapabilities(baseurl, collid):
+    # Query the collection capabilities
+    req = Request('%s/collections/%d/capabilities' %
+                  (baseurl, collid))
+
+    u = urlopen(req)
+    capab = json.loads(u.read())
+    # Check that error code is 200
+    if u.getcode() == 200:
+        return capab
+
+    raise Exception('Error retrieving capabilities.')
+
+
 class DataCollTests(unittest.TestCase):
     """Test the functionality of the Data Collection Service."""
 
@@ -61,258 +171,88 @@ class DataCollTests(unittest.TestCase):
 
     def test_coll_rule(self):
         """Rule based Collection."""
-        with open('new-coll.json') as fin:
-            data = fin.read()
-            req = Request('%s/collections' % self.host, data=data)
-            req.add_header("Content-Type", 'application/json')
-            # Create a collection
-            try:
-                u = urlopen(req)
-                coll = json.loads(u.read())
-                # Check that I received a 201 code
-                self.assertEqual(u.getcode(), 201,
-                                 'HTTP code 201 was expected!')
-            except Exception as e:
-                self.assertTrue(False, 'Error: %s' % e)
-
+        collid = createcollection(self.host, 'new-coll.json')
         with open('new-memb.json') as fin:
-            data = fin.read()
-            req = Request('%s/collections/%d/members' %
-                          (self.host, coll['collid']), data=data)
-            req.add_header("Content-Type", 'application/json')
-            # Create a member
-            try:
-                u = urlopen(req)
-                memb = json.loads(u.read())
-                # Check that I received a 201 code
-                self.assertEqual(u.getcode(), 201,
-                                 'HTTP code 201 was expected!')
-            except Exception as e:
-                self.assertTrue(False, 'Error: %s' % e)
+            memb = json.load(fin)
+        memberid = createmember(self.host, collid, 'new-memb.json')
+        memb2 = getmember(self.host, collid, memberid)
 
-            # Query the member to check it has been properly created
-            req = Request('%s/collections/%d/members/%d' %
-                                  (self.host, coll['collid'], memb['memberid']))
-            try:
-                u = urlopen(req)
-                memb2 = json.loads(u.read())
-                # Check that error code is 200
-                self.assertEqual(u.getcode(), 200,
-                                 'Error code 200 was expected!')
-                # Check that the ids are the same
-                self.assertEqual(memb['memberid'], memb2['memberid'], 'IDs differ!')
-                # Compare owner with the original one
-                msg = 'Location recorded differ with the original one!'
-                self.assertEqual(json.loads(data)['location'],
-                                 memb2['location'], msg)
-            except Exception as e:
-                self.assertTrue(False, 'Error: %s' % e)
+        # Check that the ids are the same
+        self.assertEqual(memberid, memb2['memberid'], 'IDs differ!')
+        # Compare owner with the original one
+        msg = 'Location recorded differ with the original one!'
+        self.assertEqual(memb['location'], memb2['location'], msg)
 
-        with open('new-coll-rule.json') as fin:
-            data = fin.read()
-            req = Request('%s/collections' % self.host, data=data)
-            req.add_header("Content-Type", 'application/json')
-            # Create a collection
-            try:
-                u = urlopen(req)
-                collrule = json.loads(u.read())
-                # Check that I received a 201 code
-                self.assertEqual(u.getcode(), 201,
-                                 'HTTP code 201 was expected!')
-            except Exception as e:
-                self.assertTrue(False, 'Error: %s' % e)
+        collruleid = createcollection(self.host, 'new-coll-rule.json')
+        membersrule = getmember(self.host, collruleid)
 
-            # Query all the members belonging to the collection
-            req = Request('%s/collections/%d/members/' %
-                          (self.host, collrule['collid']))
-            try:
-                u = urlopen(req)
-                memb2 = json.loads(u.read())
-                # Check that error code is 200
-                self.assertEqual(u.getcode(), 200,
-                                 'Error code 200 was expected!')
+        # FIXME What else should I check?
+        # Probably that there is only one member!
 
-                # FIXME What else should I check?
-                # Probably that there is only one member!
-            except Exception as e:
-                self.assertTrue(False, 'Error: %s' % e)
-
-            # Delete the member
-            req = Request('%s/collections/%d/members/%d' %
-                          (self.host, coll['collid'], memb['memberid']))
-            req.get_method = lambda: 'DELETE'
-            u = urlopen(req)
-            self.assertEqual(u.getcode(), 200,
-                             'HTTP Error code 200 was expected!')
-
-            # Delete the collection
-            req = Request('%s/collections/%d' %
-                                 (self.host, coll['collid']))
-            req.get_method = lambda: 'DELETE'
-            u = urlopen(req)
-            self.assertEqual(u.getcode(), 200,
-                             'HTTP Error code 200 was expected!')
-
-            # Delete the collection with rule
-            req = Request('%s/collections/%d' %
-                          (self.host, collrule['collid']))
-            req.get_method = lambda: 'DELETE'
-            u = urlopen(req)
-            self.assertEqual(u.getcode(), 200,
-                             'HTTP Error code 200 was expected!')
-            return
+        deletemember(self.host, collid, memberid)
+        deletecollection(self.host, collid)
+        deletecollection(self.host, collruleid)
+        return
 
     def test_memb_create_query_delete(self):
         """Creation, query and deletion of a Member of a Collection."""
-        with open('new-coll.json') as fin:
-            data = fin.read()
-            req = Request('%s/collections' % self.host, data=data)
-            req.add_header("Content-Type", 'application/json')
-            # Create a collection
-            try:
-                u = urlopen(req)
-                coll = json.loads(u.read())
-                # Check that I received a 201 code
-                self.assertEqual(u.getcode(), 201,
-                                 'HTTP code 201 was expected!')
-            except Exception as e:
-                self.assertTrue(False, 'Error: %s' % e)
-
+        collid = createcollection(self.host, 'new-coll.json')
         with open('new-memb.json') as fin:
-            data = fin.read()
-            req = Request('%s/collections/%d/members' %
-                          (self.host, coll['collid']), data=data)
-            req.add_header("Content-Type", 'application/json')
-            # Create a member
-            try:
-                u = urlopen(req)
-                memb = json.loads(u.read())
-                # Check that I received a 201 code
-                self.assertEqual(u.getcode(), 201,
-                                 'HTTP code 201 was expected!')
-            except Exception as e:
-                self.assertTrue(False, 'Error: %s' % e)
+            memb = json.load(fin)
+        memberid = createmember(self.host, collid, 'new-memb.json')
+        memb2 = getmember(self.host, collid, memberid)
 
-            # Query the member to check it has been properly created
-            req = Request('%s/collections/%d/members/%d' %
-                          (self.host, coll['collid'], memb['memberid']))
-            try:
-                u = urlopen(req)
-                memb2 = json.loads(u.read())
-                # Check that error code is 200
-                self.assertEqual(u.getcode(), 200,
-                                 'Error code 200 was expected!')
-                # Check that the ids are the same
-                self.assertEqual(memb['memberid'], memb2['memberid'], 'IDs differ!')
-                # Compare owner with the original one
-                msg = 'Location recorded differ with the original one!'
-                self.assertEqual(json.loads(data)['location'],
-                                 memb2['location'], msg)
-            except Exception as e:
-                self.assertTrue(False, 'Error: %s' % e)
+        # Check that the ids are the same
+        self.assertEqual(memberid, memb2['memberid'], 'IDs differ!')
+        # Compare owner with the original one
+        msg = 'Location recorded differ with the original one!'
+        self.assertEqual(memb['location'], memb2['location'], msg)
 
-            # Query all the members belonging to the collection
-            req = Request('%s/collections/%d/members/' %
-                          (self.host, coll['collid']))
-            try:
-                u = urlopen(req)
-                memb2 = json.loads(u.read())
-                # Check that error code is 200
-                self.assertEqual(u.getcode(), 200,
-                                 'Error code 200 was expected!')
+        members = getmember(self.host, collid)
 
-                # FIXME What else should I check?
-                # Probably that there is only one member!
-            except Exception as e:
-                self.assertTrue(False, 'Error: %s' % e)
+        # FIXME What else should I check?
+        # Probably that there is only one member!
 
-            # FIXME Check the syntax of "properties"
-            # Query the collection capabilities
-            # req = urllib2.Request('%s/collections/%d/capabilities' %
-            # (self.host, coll['id']))
-            # try:
-            #     u = urllib2.urlopen(req)
-            #     capab = json.loads(u.read())
-            #     # Check that capabilities have at least the maxLength field
-            #     self.assertEqual(capab['maxLength'], -1,
-            # 'maxLength is supposed to be -1 for this test!')
-            # except Exception as e:
-            #     self.assertTrue(False, 'Error: %s' % e)
+        # FIXME Check the syntax of "properties"
+        # Query the collection capabilities
+        # req = urllib2.Request('%s/collections/%d/capabilities' %
+        # (self.host, coll['id']))
+        # try:
+        #     u = urllib2.urlopen(req)
+        #     capab = json.loads(u.read())
+        #     # Check that capabilities have at least the maxLength field
+        #     self.assertEqual(capab['maxLength'], -1,
+        # 'maxLength is supposed to be -1 for this test!')
+        # except Exception as e:
+        #     self.assertTrue(False, 'Error: %s' % e)
 
-            # Delete the member
-            req = Request('%s/collections/%d/members/%d' %
-                          (self.host, coll['collid'], memb['memberid']))
-            req.get_method = lambda: 'DELETE'
-            u = urlopen(req)
-            self.assertEqual(u.getcode(), 200,
-                             'HTTP Error code 200 was expected!')
-
-            # Delete the collection
-            req = Request('%s/collections/%d' %
-                          (self.host, coll['collid']))
-            req.get_method = lambda: 'DELETE'
-            u = urlopen(req)
-            self.assertEqual(u.getcode(), 200,
-                             'HTTP Error code 200 was expected!')
-            return
+        deletemember(self.host, collid, memberid)
+        deletecollection(self.host, collid)
+        return
 
     def test_coll_create_query_delete(self):
         """Creation, query and deletion of a Collection."""
         with open('new-coll.json') as fin:
-            data = fin.read()
-            req = Request('%s/collections' % self.host, data=data)
-            req.add_header("Content-Type", 'application/json')
-            # Create a collection
-            try:
-                u = urlopen(req)
-                coll = json.loads(u.read())
-                # Check that I received a 201 code
-                self.assertEqual(u.getcode(), 201,
-                                 'HTTP code 201 was expected!')
-            except Exception as e:
-                self.assertTrue(False, 'Error: %s' % e)
+            coll = json.load(fin)
+        collid = createcollection(self.host, 'new-coll.json')
+        coll2 = getcollection(self.host, collid)
 
-            # Query the collection to check it has been properly created
-            req = Request('%s/collections/%d' %
-                                  (self.host, coll['collid']))
-            try:
-                u = urlopen(req)
-                coll2 = json.loads(u.read())
-                # Check that I received a 200 code
-                self.assertEqual(u.getcode(), 200,
-                                 'HTTP code 200 was expected!')
-                # Check that the ids are the same
-                self.assertEqual(coll['collid'], coll2['collid'], 'IDs differ!')
-                # Compare owner with the original one
-                self.assertEqual(json.loads(data)['properties']['ownership'],
-                                 coll2['properties']['ownership'],
-                                 'Owner provided differ with original one!')
-            except Exception as e:
-                self.assertTrue(False, 'Error: %s' % e)
+        # Check that the ids are the same
+        self.assertEqual(collid, coll2['collid'], 'IDs differ!')
+        # Compare owner with the original one
+        self.assertEqual(coll['properties']['ownership'],
+                         coll2['properties']['ownership'],
+                         'Owner provided differ with original one!')
 
-            # Query the collection capabilities
-            req = Request('%s/collections/%d/capabilities' %
-                                 (self.host, coll['collid']))
-            try:
-                u = urlopen(req)
-                capab = json.loads(u.read())
-                # Check that I received a 200 code
-                self.assertEqual(u.getcode(), 200,
-                                 'HTTP code 200 was expected!')
-                # Check that the capabilities have at least the maxLength field
-                self.assertEqual(capab['maxLength'], -1,
-                                 'maxLength supposed to be -1 for this test!')
-            except Exception as e:
-                self.assertTrue(False, 'Error: %s' % e)
+        # Query the collection capabilities
+        capab = getcollcapabilities(self.host, collid)
 
-            # Delete the collection
-            req = Request('%s/collections/%d' %
-                                 (self.host, coll['collid']))
-            req.get_method = lambda: 'DELETE'
-            u = urlopen(req)
-            # Check that I received a 200 code
-            self.assertEqual(u.getcode(), 200, 'HTTP code 200 was expected!')
-            return
+        # Check that the capabilities have at least the maxLength field
+        self.assertEqual(capab['maxLength'], -1,
+                         'maxLength supposed to be -1 for this test!')
+
+        deletecollection(self.host, collid)
+        return
 
 
 global host
