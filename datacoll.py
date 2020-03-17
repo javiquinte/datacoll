6f8d86bdf96684c3f2cc06376ec61a8e80ae28ff
Javier Quinteros <javier@quinteros.eu>
2020-03-17 18:00:23 -0300

    Bugfix: str or ObjectId expected instead of InsertedId

--- datacoll.py
+++ datacoll.py
@@ -326,7 +326,7 @@ class CollectionAPI(object):
         try:
             # It is important to call insert inline with an empty Collection!
             insertedid = Collection(conn, collid).insert(jsonColl)
-            coll = Collection(conn, insertedid)
+            coll = Collection(conn, str(insertedid))
         except Exception:
             # Send Error 400
             messDict = {'code': 0,
